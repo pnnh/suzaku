659e5e04beda2f8c0f409e08f2c458d316d12db3
linya <xspanni@gmail.com>
2025-02-10 21:21:07 +0800

    处理windows下编译允许出错问题，升级包版本

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,14 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-#include <quantum/quantum_plugin.h>
+#include <quantum/quantum_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-  QuantumPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("QuantumPlugin"));
+  QuantumPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuantumPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
